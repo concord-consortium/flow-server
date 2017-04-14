3448d8a8d1cafc0b87c6d8c63c9d66be9285551e
Peter Sand <petersand@users.noreply.github.com>
2017-04-13 22:32:31 -0700

    Revert "Vue.js implementation"

--- static/flow/controller-selector.js
+++ static/flow/controller-selector.js
@@ -1,53 +1,37 @@
 // initialize a view that lets the user choose a controller
-function controllerSelectorView() {
-	return Vue.component('controller-selector', {
-		template: [
-			'<div id="controllerSelectorPanel" class="flowPanel">',
-				'<div class="row">',
-					'<div class="col-md-4"></div>',
-					'<div class="col-md-4">',
-						'<h2>Select a Controller</h2>',
-						'<div class="controllerList" v-if="controllers.length > 0">',
-							'<div v-for="controller in controllers">',
-								'<button class="btn btn-lg listButton" v-on:click="viewController(controller)">{{controller.name}}</button>',
-							'</div>',
-						'</div>',
-						'<div v-else>',
-							'<input id="controller_name_entry" class="form-control" v-model="customController" />',
-							'<button class="btn btn-primary" v-on:click="fetchController()">Go</button>',
-						'</div>',
-					'</div>',
-					'<div class="col-md-4"></div>',
-				'</div>',
-			'</div>'
-		].join('\n'),
-		data: function(){
-			return {
-				controllers: g_controllers, // Populate if user is logged in
-				customController: ''
-			}
-		},
-		methods: {
-			viewController: function(controller){
-				g_controller = controller;
+function initControllerSelector() {
+	var controllerListDiv = $('#controllerList');
+	controllerListDiv.empty();
+	
+	// if we have a list of controllers, the user is logged in and can pick one of them
+	if (g_controllers.length) {
+		for (var i = 0; i < g_controllers.length; i++) {
+			var controller = g_controllers[i];
+			var div = $('<div>');
+			var button = $('<button>', {class: 'btn btn-lg listButton', html: controller.name}).appendTo(div);
+			button.click(i, function(e) {
+				g_controller = g_controllers[e.data];
 				showControllerViewer();
-			},
-			fetchController: function(){
-				$.ajax({
-					url: '/ext/flow/select',
-					method: 'POST',
-					data: {
-						controller_name: this.customController
-					},
-					success: function(data) {
-						g_controller = JSON.parse(data);
-						showControllerViewer();
-					},
-					error: function(data) {
-						alert('Controller not found.')
-					},
-				});
-			}
+			});
+			div.appendTo(controllerListDiv);
 		}
-	});
+	
+	// if not, assume user isn't logged in and allow them to type in a controller name
+	} else {
+		$('<input>', {class: 'form-control', id: 'controller_name_entry'}).appendTo(controllerListDiv);
+		$('<button>', {class: 'btn btn-primary', html: 'Go'}).appendTo(controllerListDiv).click(function() {
+			$.ajax({
+				url: '/ext/flow/select',
+				method: 'POST',
+				data: {controller_name: $('#controller_name_entry').val()},
+				success: function(data) {
+					g_controller = JSON.parse(data);
+					showControllerViewer();
+				},
+				error: function(data) {
+					alert('Controller not found.')
+				},
+			});
+		});
+	}
 }
--- static/flow/flow-app.js
+++ static/flow/flow-app.js
@@ -1,46 +1,47 @@
 // This file provide top-level control for the data flow app.
 
-var g_app;
 
 // initialize the app
 function initFlowApp() {
-	g_app = new Vue({
-		el: '#app',
-		template: [
-			'<component v-bind:is="currentView"></component>'
-		].join('\n'),
-		data: {
-			currentView: 'controllerSelector',
-		},
-		components: {
-			'controllerSelector': controllerSelectorView(),
-			'controllerViewer': controllerViewerView(),
-			'diagramEditor': diagramEditorView(),
-			'plotter': plotterView(),
-		}
-	});
+	showControllerSelector();
 }
 
 
 // change screens within the app: show the controller selection screen
 function showControllerSelector() {
-	g_app.currentView = 'controllerSelector';
+	initControllerSelector();
+	$('#controllerSelectorPanel').show();
+	$('#controllerViewerPanel').hide();
+	$('#diagramEditorPanel').hide();
+	$('#plotterPanel').hide();
 }
 
 
 // change screens within the app: show the controller contents (diagram list) screen
 function showControllerViewer() {
-	g_app.currentView = 'controllerViewer';
+	initControllerViewer();
+	$('#controllerSelectorPanel').hide();
+	$('#controllerViewerPanel').show();
+	$('#diagramEditorPanel').hide();
+	$('#plotterPanel').hide();
 }
 
 
 // change screens within the app: show the flow diagram editor screen
 function showDiagramEditor() {
-	g_app.currentView = 'diagramEditor';
+	initDiagramEditor();
+	$('#controllerSelectorPanel').hide();
+	$('#controllerViewerPanel').hide();
+	$('#diagramEditorPanel').show();
+	$('#plotterPanel').hide();
 }
 
 
 // change screens within the app: show the plotter screen
 function showPlotter() {
-	g_app.currentView = 'plotter'
+	initPlotter();
+	$('#controllerSelectorPanel').hide();
+	$('#controllerViewerPanel').hide();
+	$('#diagramEditorPanel').hide();
+	$('#plotterPanel').show();
 }
