fcf0c7ea4ece9cda5d4a1ad394c2b45717b8221a
Jason Simpson <jsimpson@concord.org>
2017-10-16 22:08:31 +0000

    Do not allow empty diagram names.
    [#151489214]

--- static/flow/flow-util.js
+++ static/flow/flow-util.js
@@ -1,6 +1,11 @@
 var Util = {};
 
 Util.diagramValidator = function(diagramName){
+
+  if(diagramName == '') {
+    return false;
+  }
+
   var forbidden = ['/', '\\', '*', '?']; // Might be easier to just ensure alphanumeracy?
 
   var hasForbidden = forbidden.some(function(c){
